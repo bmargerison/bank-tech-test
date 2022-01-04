8faf40e5c593b15ad14d254fc0afc0673b78aef9
bmargerison <bmargerison@outlook.com>
2022-01-04 11:39:05 +0000

    Removed setup file not required

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='bank_tech_test',
-      version='1.0',
-      author='Ben Margerison',
-      author_email='bmargerison@outlook.com',
-      packages=[],
-     )
