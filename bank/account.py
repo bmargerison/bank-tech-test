d23605bab34e67efdd13b5d0fe3983ec8a2672ad
bmargerison <bmargerison@outlook.com>
2022-01-04 15:17:31 +0000

    Changed stored transactions from a list to a dictionary for readability and future changes

--- bank/account.py
+++ bank/account.py
@@ -6,21 +6,25 @@ class Account:
     
     def deposit(self, amount):
         self.balance += amount
-        self.__transactions.append(['credit', amount])
+        self.__transactions.append({
+            'type': 'credit',
+            'amount': amount
+        })
 
     def withdraw(self, amount):
         self.balance -= amount
-        self.__transactions.append(['debit', amount])
+        self.__transactions.append({
+            'type': 'debit',
+            'amount': amount
+        })
 
     def view_statement(self):
         print('credit || debit || balance')
         running_total = 0
         for transaction in self.__transactions:
-            if transaction[0] == 'credit':
-                running_total += transaction[1]
-                print("£%.2f || || £%.2f" %(transaction[1], running_total))
-            elif transaction[0] == 'debit':
-                print(running_total)
-                running_total -= transaction[1]
-                print(" || £%.2f || £%.2f" %(transaction[1], running_total))
-                
+            if transaction['type'] == 'credit':
+                running_total += transaction['amount']
+                print("£%.2f || || £%.2f" %(transaction['amount'], running_total))
+            elif transaction['type'] == 'debit':
+                running_total -= transaction['amount']
+                print(" || £%.2f || £%.2f" %(transaction['amount'], running_total))
