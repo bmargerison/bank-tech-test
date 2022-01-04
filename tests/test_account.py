6f31b40c04c48856b08a7213a13e46651ccdd9dc
bmargerison <bmargerison@outlook.com>
2022-01-04 12:41:42 +0000

    User story 3 - withdraw unit test

--- tests/test_account.py
+++ tests/test_account.py
@@ -8,3 +8,9 @@ def test_deposit():
     account = Account()
     account.deposit(20)
     assert account.balance == 20
+
+def test_withdraw():
+    account = Account()
+    account.deposit(20)
+    account.withdraw(5)
+    assert account.balance == 15
