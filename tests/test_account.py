2fc6bd1a2281799364951c33edfbefa30cd9566d
bmargerison <bmargerison@outlook.com>
2022-01-04 12:35:47 +0000

    Second user story - deposit unit test

--- tests/test_account.py
+++ tests/test_account.py
@@ -4,3 +4,7 @@ def test_account():
     account = Account()
     assert isinstance(account, Account)
 
+def test_deposit():
+    account = Account()
+    account.deposit(20)
+    assert account.balance == 20
