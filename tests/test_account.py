1ac3d6e2bfc0617e2a0e159ddec050b46a52c2d4
bmargerison <bmargerison@outlook.com>
2022-01-04 12:55:54 +0000

    User story 4 - view statement unit test using capfd fixture

--- tests/test_account.py
+++ tests/test_account.py
@@ -14,3 +14,16 @@ def test_withdraw():
     account.deposit(20)
     account.withdraw(5)
     assert account.balance == 15
+
+def test_display_balance(capfd):
+    account = Account()
+    account.deposit(20)
+    account.withdraw(5)
+    account.view_statement
+    statement, err = capfd.readouterr()
+    assert "deposit" in statement
+    assert "withdrawal" in statement
+    assert "balance" in statement
+    assert "£15.00" in statement
+
+
