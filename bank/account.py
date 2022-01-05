dfea8a34c90cc3265bff0b42f15b1c2b633456ee
bmargerison <bmargerison@outlook.com>
2022-01-05 11:47:01 +0000

    Refactor to remove add_balance() function

--- bank/account.py
+++ bank/account.py
@@ -13,7 +13,8 @@ class Account:
         self.__transactions.append({
             'type': 'cr',
             'amount': amount,
-            'date': datetime.datetime.now().strftime('%d/%m/%Y')
+            'date': datetime.datetime.now().strftime('%d/%m/%Y'),
+            'balance': self.balance
         })
 
     def withdraw(self, amount):
@@ -23,26 +24,17 @@ class Account:
         self.__transactions.append({
             'type': 'dr',
             'amount': amount,
-            'date': datetime.datetime.now().strftime('%d/%m/%Y')
+            'date': datetime.datetime.now().strftime('%d/%m/%Y'),
+            'balance': self.balance
         })
 
     def view_statement(self):
         print('date || credit || debit || balance')
-        transactions = self.__add_balance()
+        transactions = self.__transactions[::-1]
         for tr in transactions:
             print_tr = (tr['date'], tr['amount'], tr['balance'])
             if tr['type'] == 'cr': print("%s || £%.2f || || £%.2f" %print_tr)
             elif tr['type'] == 'dr': print("%s || || £%.2f || £%.2f" %print_tr)
-
-    def __add_balance(self):
-        balance = 0
-        transactions = []
-        for tr in self.__transactions:
-            if tr['type'] == 'cr': balance += tr['amount']
-            if tr['type'] == 'dr': balance -= tr['amount']
-            tr['balance'] = balance
-            transactions.append(tr)
-        return transactions[::-1]
 
     def __validate_number(self, number):
         if not isinstance(number, (int, float)): 
